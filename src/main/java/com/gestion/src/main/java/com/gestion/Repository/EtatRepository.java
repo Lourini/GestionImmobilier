9fd6376ac99884dc22b165dc526c992b86eff30b
Othmane Lourini <61596340+Lourini@users.noreply.github.com>
2023-09-02 12:15:38 +0100

    Delete src/main/java/com/gestion/src directory

--- src/main/java/com/gestion/src/main/java/com/gestion/Repository/EtatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestion.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gestion.domain.Etat;
-
-
-public interface EtatRepository extends JpaRepository<Etat, Long> {
-}
